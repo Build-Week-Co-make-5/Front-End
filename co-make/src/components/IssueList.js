0db27e110b99545f9cf9e413918a46e9ce29e27d
Catherine Kim <catherinesjkim@gmail.com>
2020-03-05 20:10:34 -0800

    Add updated files.

--- co-make/src/components/IssueList.js
+++ co-make/src/components/IssueList.js
@@ -5,22 +5,9 @@ import axios from "axios";
 import "../App.css";
 // import { Link } from "react-router-dom"; moved to SearchForm
 import { Route } from "react-router-dom"; //created new route
-// import Image1 from "./Image1/img_1.jpg";
-// import Image2 from "./Image2/Image2/img_2.jpg";
-// import Image3 from "./Image3/img_3.jpg";
-// import Image4 from "./Image4/img_4.jpg";
-// import Image5 from "./Image5/img_5.jpg";
-// import Image6 from "./Image6/img_6.jpg";
 // import { Link } from "react-router-dom"; // moved to SearchForm
 import Issue from "./Issue";
 import Images from "./Images/img_1.jpg"; 
-
-// console.log(Image1);
-// console.log(Image2);
-// console.log(Image3);
-// console.log(Image4);
-// console.log(Image5);
-// console.log(Image6);
 
 function IssueList() {
   const [issues, setIssues] = useState([]);
@@ -53,12 +40,6 @@ function IssueList() {
     (
       <div className="issue-list">
         <img src={Images} alt="issue images" />
-        {/* <img src={Image1} alt="issue image1" />
-        <img src={Image2} alt="issue image2" />
-        <img src={Image3} alt="issue image3" />
-        <img src={Image4} alt="issue image4" />
-        <img src={Image5} alt="issue image5" />
-        <img src={Image6} alt="issue image6" /> */}
         <div>
           <SearchForm issues={issues} />
           {/* added a Route to render each individual issue page by ID */}
