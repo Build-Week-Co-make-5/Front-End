26f2eb795a9ed480b15b062fb5094018ee619ed6
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-03-07 00:13:53 +0800

    fixed merge conflict in Issue.js

--- co-make/src/components/Issue.js
+++ co-make/src/components/Issue.js
@@ -5,22 +5,6 @@ const Issue = (props) => {
   const [upvotes, setUpvotes] = useState(0);
   const [disabled, setDisabled] = useState(false);
 
-<<<<<<< HEAD
-  // commented out all code below as it's not working, will fix after everything else is done
-
-  // create a paramItemId variable to match to respective :id URLs
-  // console.log(props.match.params.id);
-  // const paramItemId = props.match.params.id;
-
-  // // matches each issue id to its params id
-  // const issue = props.data.find(issue => {
-  //   return issue.id === Number(paramItemId);
-  // });
-
-  // console.log("issue found!", issue);
-
-=======
->>>>>>> a47b4d8a7d81ea66135c2e104d1e4c6305f65a58
   return (
     // refactored the code below to reflect each ID's issue props ***
     <div className="issue-card">
